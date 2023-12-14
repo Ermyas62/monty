5147bdb53dc7eb2a78e3c674474aec1293be773d
Ermyas62 <ermyas62@gmail.com>
2023-12-14 07:21:29 -0800

    monty

--- push.c
+++ push.c
@@ -26,8 +26,7 @@ void _push(stack_t **head, unsigned int counter)
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}}
+		exit(EXIT_FAILURE); }}
 	else
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", counter);
